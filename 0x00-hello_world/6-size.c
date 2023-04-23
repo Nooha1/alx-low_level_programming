d2a3c351959c1ddbae5acd0192d265a3bb4ce8b9
Nohaila <ezz.nohaila@gmail.com>
2023-04-23 12:35:34 -0700

    types various

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("Size of a char: \'$sizeof(char)'byte(s)");
+	printf("Size of an int: \'$sizeof(int)'byte(s)");
+	printf("Size of a long int: \'$sizeof(long int)'byte(s)");
+	printf("Size of a long long int: \'$sizeof(long long int)'byte(s)");
+	printf("Size of a float: \'$sizeof(float)'byte(s)");
+	return (0);
+}
