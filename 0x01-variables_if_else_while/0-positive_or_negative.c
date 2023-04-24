ba2ef908f9259270ec505e2d290a76ca8401a518
Nohaila <ezz.nohaila@gmail.com>
2023-04-24 14:09:51 -0700

    add positive or negative file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
 
@@ -10,10 +11,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("%d is positive\n");
+		printf("%d is positive\n", n);
 	else if (n == 0)
-		printf("%d is zero\n");
+		printf("%d is zero\n", n);
 	else
-		printf("%d is negative\n");
+		printf("%d is negative\n", n);
 	return (0);
 }
