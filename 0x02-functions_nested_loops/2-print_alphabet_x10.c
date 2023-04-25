69bde0ae5e3a2d73505bf0a7147771401e63c1ca
nohaila <ezz.nohaila@gmail.com>
2023-04-25 16:37:39 -0700

    add print alphabet x10 file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include "1-alphabet.c"
+
+/**
+ * print_alphabet_x10 - print alphabet x 10
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		print_alphabet();
+	}
+	putchar('\n');
+}
