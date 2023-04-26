00b250e87e262dfcd6d235efdc13d61d88a74a1e
nohaila <ezz.nohaila@gmail.com>
2023-04-26 13:25:04 -0700

    add isalpha file

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isalpha - isalpha function.
+ * @c: character to be checked
+ *
+ * Return: 1 if letter, otherwise 0.
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' || c >= 'A') && (c <= 'z' || c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
