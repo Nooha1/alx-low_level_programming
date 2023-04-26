e064230aef4a61af4553018a85282433c68cd36c
nohaila <ezz.nohaila@gmail.com>
2023-04-26 15:05:45 -0700

    add abs file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _abs - function computs the absulute value of an integer.
+ * @r:^parameter used.
+ *
+ * Return: 0.
+ */
+int _abs(int r)
+{
+	_abs(r);
+	_putchar(r);
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/6-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int r;
+	int n = 1;
+
+	r = _abs(n);
+	printf("%d\n", r);
+	return (0);
+}
