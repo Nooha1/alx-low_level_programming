5ef7a352cfc915997261822a1019a889f4e5df26
nohaila <ezz.nohaila@gmail.com>
2023-04-28 09:50:27 -0700

    add uppercase file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * _isupper - print uppercase
+ * @c: parameter char
+ *
+ * Return: 1 if upper, otherwise 0.
+ */
+int _isupper(int c)/* _isupper - print uppercase */
+	/* @c: parameter char*/
+	 /* Return: 1 if upper, otherwise 0.*/
+{
+	if (c <= 'A' && c >= 'Z')
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Alway0.
+ */
+int main(void)
+{
+	char c = 'A';
+
+	printf("%c: %d\n", c, _isupper(c));
+	return (0);
+}
