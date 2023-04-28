cb0106f6f946106fb02ab2698a57019f24202dbe
nohaila <ezz.nohaila@gmail.com>
2023-04-28 12:08:48 -0700

    add positive or negative file

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,15 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
 
-it main(void)
+int main(void)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
 	positive_or_negative(i);
 	
 	return (0);
