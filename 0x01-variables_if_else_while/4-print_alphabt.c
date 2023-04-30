18a9bf1e2064cc6d8c75626e098e19d82a111f14
nohaila <ezz.nohaila@gmail.com>
2023-04-30 16:23:46 -0700

    add alphabet file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main- print alphabets except q and e
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+		putchar(c);
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
