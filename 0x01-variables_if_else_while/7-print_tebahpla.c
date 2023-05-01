b2ccfb23a199d75641516f558c0efbf9c016f445
nohaila <ezz.nohaila@gmail.com>
2023-05-01 03:35:18 -0700

    add tebahpla file

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print tebahpla
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
