edd54db23ff70225c8c5320bddd1277dc3862cb0
nohaila <ezz.nohaila@gmail.com>
2023-05-07 14:11:33 -0700

    add multiples file

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - multiples two integers
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a = 3; int b = 5;
+
+	printf("%d\n", mul(a, b));
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - multiples two integers.
+ * @a: parameters used
+ * @b: parameters used
+ *
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+	/**
+	 * @a: parameters used.
+	 * @b: parameters used
+	 */
+{
+	return (a * b);
+}
+
