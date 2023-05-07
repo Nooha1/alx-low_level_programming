529241648be974e4290f64b6934bf28588eb7a67
nohaila <ezz.nohaila@gmail.com>
2023-05-06 18:55:17 -0700

    add upper file

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,12 +6,19 @@
  *
  * Return: 1 if upper, otherwise 0.
  */
-int _isupper(int c)/* _isupper - print uppercase */
+int _isupper(int c)
+	/** _isupper - print uppercase */
 	/* @c: parameter char*/
-	 /* Return: 1 if upper, otherwise 0.*/
+	/* Return: 1 if upper, otherwise 0.*/
 {
 	if (c <= 'A' && c >= 'Z')
+	{
 		return (1);
+		printf("n");
+	}
 	else
+	{
 		return (0);
+		printf("n");
+	}
 }
